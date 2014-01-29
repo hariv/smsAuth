a24cd9124b3ab738b54b10067cdd543f79057e4a
root <root@hari-Dell-System-XPS-L502X.(none)>
2014-01-29 18:52:12 +0530

    Canvas Ready

--- main.js
+++ main.js
@@ -1,22 +1,51 @@
-(function(){
-    var streaming=false,
-    video=document.querySelector('videoStream'),
-    canvas=document.querySelector('snapShot'),
-    button=document.querySelector('takePhoto'),
-    width=320,
-    height=0;
-    navigator.getMedia=(navigator.getUserMedia||navigator.webkitGetUserMedia||navigator.mozGetUserMedia||navigator.msGetUserMedia);
-    navigator.getMedia({video: true,audio: false},function(stream){
-	if(navigator.mozGetUserMedia)
-	{
-	    video.mozSrcObject=stream;
-	}
-	else
-	{
-	    var vendorURL=window.URL||window.webkitURL;
-	    video.src=vendorURL.createObjectURL(stream);
-	}
-    },function(err){
-	console.log(err);
-    });
-})();
+function snap(w,h)
+{
+    console.log("Called Snap");
+    var live=document.getElementById('webcam');
+    var snapShot=document.getElementById('snapShot');
+    snapShot.width=w;
+    snapShot.height=h;
+    c=snapShot.getContext("2d");
+    c.drawImage(live,0,0,w,h);
+    var comp=ccv.detect_objects({"canvas" : (ccv.pre(snapShot)),"cascade" : cascade,"interval" : 5,"min_neighbors" : 1 });
+    console.log(comp);
+}
+function onSuccess(stream) 
+{
+    var video = document.getElementById('webcam');
+    var videoSource;
+    var ratio, w,h;
+    if(window.webkitURL)
+	videoSource=window.webkitURL.createObjectURL(stream);
+    else
+	videoSource=stream;
+    video.autoplay=true;
+    video.src=videoSource;
+    video.addEventListener('loadedmetadata',function(){
+	console.log("Loaded Metadata");
+	ratio=video.videoWidth/video.videoHeight;
+	w=video.videoWidth-100;
+	h=parseInt(w/ratio,10);
+	snap(w,h);
+    },false);
+}
+function onError(err) 
+{
+    console.log(err);
+    alert('There has been a problem retrieving the streams - did you allow access?');
+}
+window.onload=function()
+{
+    var video=document.getElementById('videoStream'),
+    canvas=document.getElementById('snapShot');
+    navigator.getUserMedia || (navigator.getUserMedia = navigator.mozGetUserMedia || navigator.webkitGetUserMedia || navigator.msGetUserMedia);
+    if (navigator.getUserMedia)
+    {
+	navigator.getUserMedia({
+	    video: true,
+	    audio: false
+	},onSuccess,onError);
+    }
+    else 
+	alert('getUserMedia is not supported in this browser.');
+}
