47c2e8288ca1734d76c763b75089cf5b5361030f
root <root@hari-Dell-System-XPS-L502X.(none)>
2014-01-29 18:53:56 +0530

    Done with shell

--- sendSms.php
+++ sendSms.php
@@ -1,15 +1,19 @@
 <?php
- if(!isset($_POST['phoneNumber']) || !isset($_POST['name']))
+/* if(!isset($_POST['phoneNumber']) || !isset($_POST['name']))
  {
     echo "Invalid da!!";
     exit(0);
- }
+ }*/
  require_once('unirest-php/lib/Unirest.php');
+ /*require_once('unirest-php/lib/Unirest.php');
  $phoneNumber=$_POST['phoneNumber'];
  $name=$_POST['name'];
  $time=time();
  $smsResult=md5($phoneNumber.$name.$time);
  $url="https://site2sms.p.mashape.com/index.php?uid=8348121945&pwd=330666&phone=8144064012&msg=".$smsResult;
  $response=Unirest::get($url,array("X-Mashape-Authorization"=>"hjKGftDlUAZkFZ2ZjWjtMAIjpJu6CVzL"),null);
- echo $response->code;
+ echo $response->code;*/
+  $output= shell_exec("curl --include --request GET 'https://site2sms.p.mashape.com/index.php?uid=8438121945&pwd=330666&phone=8144064012&msg=Mahadev' --header 'X-Mashape-Authorization: hjKGftDlUAZkFZ2ZjWjtMAIjpJu6CVzL'");
+ echo $output;
+/* $output=Unirest::get("https://site2sms.p.mashape.com/index.php?uid=8438121945&pwd=330666&phone=8144064012&msg=Thanks",array("X-Mashape-Authorization" => "hjKGftDlUAZkFZ2ZjWjtMAIjpJu6CVzL"),null);*/
 ?>
